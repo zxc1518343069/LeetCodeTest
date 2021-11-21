77359d7f7814aed3ac0051e13f0797aa3f216d29
1518343069@qq.com <1518343069@qq.com>
2021-11-21 23:34:23 +0800

    2021.11.21 我是废物  第五题坑死我了 今天是回文数 这个js写题真得考虑大数 一些就得上BigInt

---
+++ 算法通关之路/回文数/906. 超级回文数.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} left
+ * @param {string} right
+ * @return {number}
+ */
+
+// 暴力
+var superpalindromesInRange = function(left, right) {
+  let res = 0
+  const Palindrome = (s) => {
+    let left = 0, right = s.length - 1
+    while (left < right) {
+      if (s[left] !== s[right]) {
+        return false
+      }
+      left++
+      right--
+    }
+    return true
+  }
+  for (let i = Math.floor(parseInt(left) ** 0.5); i < Math.ceil(parseInt(right) ** 0.5); i++) {
+    if (Palindrome(BigInt(i).toString()) && Palindrome((BigInt(i) ** BigInt(2)).toString())) {
+      res++
+    }
+  }
+  return res
+};
+
+// console.log(superpalindromesInRange('4', '1000'))
+// console.log(superpalindromesInRange('1', '2'))
+console.log(superpalindromesInRange('40000000000000000', '50000000000000000'))
