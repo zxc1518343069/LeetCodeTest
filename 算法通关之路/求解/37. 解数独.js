ae0b7d628bfffbea31205449b4bffac24016bc27
1518343069@qq.com <1518343069@qq.com>
2021-11-28 20:56:47 +0800

    2021.11.28 我这个 智商是不是不适合刷题啊

---
+++ 算法通关之路/求解/37. 解数独.js
@@ -0,0 +1,10 @@
+// https://leetcode-cn.com/problems/sudoku-solver/
+
+// 2021 11 28 困难先跳过
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function(board) {
+
+};
---
+++ 算法通关之路/求解/679. 24 点游戏.js
@@ -0,0 +1,3 @@
+// https://leetcode-cn.com/problems/24-game/
+
+// 2021 11 28 困难先跳过
