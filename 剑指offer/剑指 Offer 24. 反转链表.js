648658f049b5ccdcbefaec6269b49ea10b6fc4fd
achen <1518343069@qq.com>
2022-07-05 09:19:02 +0800

    2022.7.5 清晨理解一道递归版本的 链表反转。我好菜

--- 剑指offer/剑指 Offer 24. 反转链表.js
+++ 剑指offer/剑指 Offer 24. 反转链表.js
@@ -21,3 +21,16 @@ var reverseList = function(head) {
   }
   return q
 };
+
+// 递归 方式 如果理解不了就画图看下
+//在每次 函数执行 都把 head.next指向空，下一个节点指向自己。但是链接他的节点指向并没有改变。
+// 所以可以一直找到 指向null 的节点 直到最后
+reverseList = function(head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  const newHead = reverseList(head.next);
+  head.next.next = head; //  吧 head.next 看做成一个整体就可以了
+  head.next = null;
+  return newHead;
+};
