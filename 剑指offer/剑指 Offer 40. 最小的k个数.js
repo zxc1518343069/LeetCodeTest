d6aa511aec6ae7a4ab82960a82c7c6b40cab12d2
achen <1518343069@qq.com>
2022-07-06 22:35:23 +0800

    测试了一下vscode ,打算以后转型vscode

--- 剑指offer/剑指 Offer 40. 最小的k个数.js
+++ 剑指offer/剑指 Offer 40. 最小的k个数.js
@@ -1,4 +1,4 @@
-// https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+//* https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
 
 /**
  * @param {number[]} arr
@@ -7,14 +7,13 @@
  */
 
 // 直接排序
-const getLeastNumbers = function(arr, k) {
+const getLeastNumbers = function (arr, k) {
   return arr.sort((a, b) => a - b).slice(0, k);
 };
 
 // 堆
-const getLeastNumbers2 = function(arr, k) {
+const getLeastNumbers2 = function (arr, k) {
   return arr.sort((a, b) => a - b).slice(0, k);
 };
 
-
-console.log(getLeastNumbers(arr = [3, 2, 1], k = 2))
+console.log(getLeastNumbers((arr = [3, 2, 1]), (k = 2)));
