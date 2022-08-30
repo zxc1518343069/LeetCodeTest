0c48957022302180e691f7d75cbae065357f1a2b
achen <1518343069@qq.com>
2022-08-31 00:02:50 +0800

    update: 赶着凌晨再A一个题目，虽然就是简单的while

--- 剑指offerⅡ/剑指 Offer II 074. 合并区间.js
+++ 剑指offerⅡ/剑指 Offer II 074. 合并区间.js
@@ -3,6 +3,8 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
+// 这个思路好像没什么特别的了
+// 循环找就可以了
 var merge = function (intervals) {
   if (intervals == null || !intervals.length) {
     return intervals;
@@ -11,4 +13,29 @@ var merge = function (intervals) {
   intervals.sort((a, b) => {
     return a[0] - b[0];
   });
+  const intervalsLen = intervals.length;
+  let result = [];
+  let i = 0;
+  while (i < intervalsLen) {
+    let j = i + 1;
+    const cur = [...intervals[i]];
+    // 判断中止大于起始位
+    while (j < intervals.length && cur[1] >= intervals[j][0]) {
+      // 合并 并取最大值 结尾值
+      cur[1] = Math.max(cur[1], intervals[j][1]);
+      j++;
+    }
+    result.push(cur);
+    i = j;
+  }
+  return result;
 };
+
+console.log(
+  merge([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ])
+); // [ [ 1, 6 ], [ 8, 10 ], [ 15, 18 ] ]
