de896397b15ba1b4604df8dad801d0c4bd3f29dc
achen <1518343069@qq.com>
2022-11-20 22:05:15 +0800

    update: 借了一台电脑使用，

--- 剑指offerⅡ/剑指 Offer II 119. 最长连续序列.js
+++ 剑指offerⅡ/剑指 Offer II 119. 最长连续序列.js
@@ -4,25 +4,41 @@
  * @return {number}
  */
 
-//set方法
-var longestConsecutive = function(nums) {
-  const set = new Set(nums) // set存放数组的全部数字
-  let max = 0
+//set方法 2022-11-20 更新
+var longestConsecutive = function (nums) {
+  const set = new Set(nums); // set存放数组的全部数字
+  let max = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (!set.has(nums[i] - 1)) { // nums[i]没有左邻居，是序列的起点
-      let cur = nums[i]
-      let count = 1
-      while (set.has(cur + 1)) { // cur有右邻居cur+1
-        cur++ // 更新cur
-        count++
+    if (!set.has(nums[i] - 1)) {
+      // 先找到最小的数
+      let count = 1,
+        number = nums[i];
+      while (set.has(number + 1)) {
+        number++;
+        count++;
       }
-      max = Math.max(max, count) // cur不再有右邻居，检查count是否最大
+      max = Math.max(max, count);
     }
   }
-  return max
-
+  return max;
 };
+// longestConsecutive = function (nums) {
+//   const set = new Set(nums); // set存放数组的全部数字
+//   let max = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!set.has(nums[i] - 1)) {
+//       // nums[i]没有左邻居，是序列的起点
+//       let cur = nums[i];
+//       let count = 1;
+//       while (set.has(cur + 1)) {
+//         // cur有右邻居cur+1
+//         cur++; // 更新cur
+//         count++;
+//       }
+//       max = Math.max(max, count); // cur不再有右邻居，检查count是否最大
+//     }
+//   }
+//   return max;
+// };
 
-console.log(
-    longestConsecutive(nums = [100, 4, 200, 1, 3, 2])
-) // 4
+console.log(longestConsecutive((nums = [100, 4, 200, 1, 3, 2]))); // 4
