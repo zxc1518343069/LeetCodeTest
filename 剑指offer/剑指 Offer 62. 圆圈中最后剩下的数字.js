02b5f9145fa5fb9f246ac93c41f5b67845df6ecf
achen <1518343069@qq.com>
2022-11-23 00:04:24 +0800

    update: 简单题真不会做

--- 剑指offer/剑指 Offer 62. 圆圈中最后剩下的数字.js
+++ 剑指offer/剑指 Offer 62. 圆圈中最后剩下的数字.js
@@ -4,7 +4,15 @@
  * @param {number} m
  * @return {number}
  */
-
-var lastRemaining = function(n, m) {
-
+// 1 2 3 4 5 6 7 8  len = 8
+// 1 2 4 5 6 7 8  n = 7  pos = 3 index = 2  i = 0
+// 1 2 4 5 6 8  n = 6  pos = 7   index =    i = 1
+// 1 2 4 5 6 8  n = 5  pos = 2
+var lastRemaining = function (n, m) {
+  // todo 真没理解
+  let pos = 0;
+  for (let i = 2; i <= n; i++) {
+    pos = (pos + m) % i;
+  }
+  return pos;
 };
