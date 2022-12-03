ce761b1f204feab73de5d655b3ad1b9a36fce27b
achen <1518343069@qq.com>
2022-12-03 21:46:31 +0800

    update: 后面的也刷不动了。不如重新来一次。

---
+++ 2022-12左神/sort/selectSort.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param {number[]} nums
+ */
+// 选择排序
+function selectSort(nums) {
+  if (!nums || !nums.length || nums.length < 2) {
+    return [];
+  }
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    let minIndex = i;
+    // 找到最小的数字下标
+    for (let j = i + 1; j < len; j++) {
+      if (nums[minIndex] > nums[j]) {
+        minIndex = j;
+      }
+    }
+    // 交换位置
+    [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
+  }
+  return nums;
+}
+
+console.log(selectSort([1, 7, 3, 0, 6, 4, 7, 1, 8, 2]));
